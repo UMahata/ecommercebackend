605544f0caf40a94108a8dd1263fe4d3bd4aac9c
UTPAL MAHATA <mahatautpalrc6@gmail.com>
2024-08-23 16:11:01 +0530

    ready for stripe synk k4

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,13 +1,12 @@
 const express = require('express')
-const { fetchUserById, updateUser } = require('../controller/User')
-const { createUser ,loginUser, checkUser } = require('../controller/Auth')
+const { createUser ,loginUser,  checkAuth } = require('../controller/Auth')
 const passport = require('passport')
 const router = express.Router()
 
 router
       .post('/signup',createUser)
       .post('/login',passport.authenticate("local"), loginUser)
-      .get('/check',passport.authenticate("jwt"), checkUser)
+      .get('/check',passport.authenticate("jwt"), checkAuth)
       
 
 exports.router = router
